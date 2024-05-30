9e437b2dab1bc94b763e576de1f5b64f1c086148
Alexia Mata <alexia.i.mata.perez@outlook.com>
2024-05-30 17:11:09 -0600

    Solucion del ejercicio

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
 const itemsContainer = document.querySelector("#list-items");
+let lSArray = [];
+
+let removeBtn = document.createElement('button');
+removeBtn.className = 'btn btn-danger';
+removeBtn.innerText = 'Remover registro';
+removeBtn.style.width = '50%';
+removeBtn.style.margin = '3%';
+
+let loadBtn = document.createElement('button');
+loadBtn.className = 'btn btn-success';
+loadBtn.innerText = 'Cargar registro';
+loadBtn.style.width = '50%';
+loadBtn.style.margin = '3%';
+
+itemsContainer.insertAdjacentElement('beforeend', removeBtn);
+itemsContainer.insertAdjacentElement('beforeend', loadBtn);
+
+removeBtn.addEventListener('click', removeItemslS);
+loadBtn.addEventListener('click', loadColorsFromStorage)
+
 function addItem(item) {
     const itemHTML = '<div class="card" style="width: 18rem;">\n' +
         '    <div class="card-body">\n' +
@@ -14,10 +34,34 @@ function addItem(item) {
 
 // after fetching the colors, call addItem with each color
 function fetchColorsList() {
-  
+    const promesa = fetch ('https://reqres.in/api/unknown', {method : 'GET'});
+    promesa
+    .then( (response) => {
+        response.json().then( (colorsObj) => {
+            colorsObj.data.forEach(element => {
+                lSArray.push(element);
+            });
+            localStorage.setItem('colors', JSON.stringify(lSArray))
+        } )
+        .catch(
+            (error) => {console.log('Problema con el json ' + error);}
+        )
+    })
+    .catch((err) => { console.log('Existio un problema con la solicitud' + err);});
 }//fetchColorsList
 
-function loadColorsFromStorage() {
-  
-}//loadColorsFromStorage
 
+fetchColorsList()
+function loadColorsFromStorage() {
+  let array = JSON.parse(localStorage.getItem('colors'));
+  array.forEach(element => {
+        addItem(element)
+  });
+}
+
+function removeItemslS() {
+    localStorage.removeItem('colors');
+    itemsContainer.innerHTML = '';
+    itemsContainer.insertAdjacentElement('beforeend', removeBtn);
+    itemsContainer.insertAdjacentElement('beforeend', loadBtn);
+}
